8dbd7ca5183384820c56b77fa4fa306ba12e48f3
Ferry Ayunda <135429317+kisahklasik@users.noreply.github.com>
2023-12-25 20:55:03 +0700

    Update app.js

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ Auto
 <p class="card-text">
 <ol>
 <li>Klik Tombol <strong>Pilih File</strong> unggah foto terbaik kamu.</li>
-<li>Kamu bisa melakukan <strong>zoom</strong>, <strong>perbesar</strong> atau <strong>perkecil</strong> dan <strong>geser foto</strong>s.</li>
+<li>Kamu bisa melakukan <strong>zoom</strong>, <strong>perbesar</strong> atau <strong>perkecil</strong> dan <strong>geser foto</strong>.</li>
 <li>Jika sudah sesuai, klik tombol <strong>Download</strong>.</li>
 <li>Silahkan <strong><em>Bagikan</em></strong> ke sosial media kamu.</li>
 <li>Selesai.</li>
