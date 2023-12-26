625a8e68a8ad9546c1c8178f80bc68ca6cd49911
Ferry Ayunda <135429317+kisahklasik@users.noreply.github.com>
2023-12-26 21:27:45 +0700

    Update app.js

--- app.js
+++ app.js
@@ -99,13 +99,6 @@ Auto
 </div>
 </div>
 
-
-<div id="alertError" class="alert hidden" role="alert">
-  <span class="closebtn">&times;</span>  
-  <strong>Danger!</strong> Indicates a dangerous or potentially negative action.
-</div>
-
-
 <footer class="pt-3 mt-4 text-body-secondary border-top">
 Copyright &copy; <span id="copyright"></span> <a href="https://www.facebook.com/bungferry.wa" class="text-decoration-none">Ferry Ayunda</a>.
 </footer>
